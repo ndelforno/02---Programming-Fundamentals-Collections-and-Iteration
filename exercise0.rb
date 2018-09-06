509bdfab54a689d3dd5340262fc54e1d1b7cc98c
Ndelforno <nicolas.delforno@gmail.com>
2018-09-06 13:38:18 -0400

    exercise 0 done

---
+++ exercise0.rb
@@ -0,0 +1,29 @@
+fav_colours = ["black", "white", "green"]
+age_sibling = ["63", "30", "89"]
+flip_a_coin = [true, false, false, true, false]
+fav_artists = ["booba", "snoop dogg", "michael jackson"]
+fav_colours_symb = [:black, :white, :green]
+
+dictionary = {
+  :beautifful => "something pretty",
+  :stadium => "field for profesionnal players",
+  :car => "locomotion vehicule"
+}
+
+fav_movies = {
+  :fight_club => 1999,
+  :the_godfather => 1972,
+  :american_gangster => 2007
+}
+
+world_cities = {
+  :toronto => 7000000,
+  :nice => 400000,
+  :antibes => 80000
+}
+
+siblings = {
+  :mom => 63,
+  :girlfriend => 30,
+  :grandmother => 89
+}
