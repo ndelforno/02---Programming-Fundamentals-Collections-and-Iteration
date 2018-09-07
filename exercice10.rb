21fb251b98eb3d7dd69f454424c06ac2baa1910a
Ndelforno <nicolas.delforno@gmail.com>
2018-09-07 09:06:22 -0400

    exercise 11 done

---
+++ exercice10.rb
@@ -0,0 +1,13 @@
+one_to_hundred = (1..100)
+
+one_to_hundred.each do |num|
+  if (num % 3) == 0 && (num % 5 == 0)
+    puts "BitMaker"
+  elsif (num % 5) == 0
+      puts "Maker"
+  elsif (num % 3) == 0
+    puts "Bit"
+  else
+    puts num
+  end
+end
